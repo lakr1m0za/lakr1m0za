e3b69788452454062be18e9ab0e408b5a6df5355
Lakr1m0za <mohax8romeo@gmail.com>
2020-11-22 18:40:34 +0200

    Add bash simple calculator

---
+++ Ansible_task/Bash/all in one/main_wos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#without spaces
+while read; do
+        [ "$REPLY" = exit ] && echo bye && exit
+        [[ "$REPLY" =~ ^-?[0-9]+(\*\*|[/*+-])-?[0-9]+$ ]] && echo $((REPLY))
+done
+echo error
---
+++ Ansible_task/Bash/function/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello!"
+echo "This is a simple bash calculator."
+echo "You can count math operations like this:"
+echo "5+2 , 3-7 , 4*1 , 9/6"
