5dfb42083f0e6e7812f009332bff92f436f8de2a
Lakr1m0za <mohax8romeo@gmail.com>
2020-11-25 01:18:05 +0200

    Update test script.

--- Bash/all in one/test.sh
+++ Bash/all in one/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 function checkNumber {
-    regExp='^[+-]?([0-9]+\.?|[0-9]*\.[0-9]+)(\*\*|[/*+-])([0-9]+\.?|[0-9]*\.[0-9]+)$'
+    regExp='^[+-]?([0-9]*\.?|[0-9]*\.[0-9]*)(\*\*|[\/*+-])([0-9]*\.?|[0-9]*\.[0-9]*)$'
     if [[ $testNo =~ $regExp ]]
     then
-        printf '%.1f' "$((10**3 * testNo))e-3"
+        printf '%.1f' "$((10**3 * $testNo))e-3"
+        printf "\n"
+        echo $((testNo))
         let check=1
     else
         echo $((testNo))
@@ -14,4 +16,6 @@ checkNumber
 testNo=0.5+0.5
 checkNumber
 testNo=.5*.5
+checkNumber
+testNo=5.-2.
 checkNumber
