e34705acb5c05ae21f48ad823f2af67f104840ce
Lakr1m0za <mohax8romeo@gmail.com>
2020-12-10 00:34:06 +0200

    Test all functions

--- Bash/division.sh
+++ Bash/division.sh
@@ -4,6 +4,6 @@ source parse.sh
 
 function division {
     parse $n1 $n3
-    printf %.2f "$((10**2 * $n10 $n2 $n30))e-2"
+    printf %.2f "$((10**2 * $n10 / $n30))e-2"
     printf "\n"
 }
--- Bash/subtraction.sh
+++ Bash/subtraction.sh
@@ -4,7 +4,7 @@ source parse.sh
 
 function subtraction {
     parse $n1 $n3
-    result=$(($n10 $n2 $n30))
-    printf %.2f "$((10**2 * $result/100))e-2"
+    result=$(($n10 "-" $n30))
+    printf %.2f "$((10**2 * $result/10))e-2"
     printf "\n"
 }
